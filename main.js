8528c5dfc3e1547dd9192816aaa9b304eff9a79b
Jeremy Pearson <jeremy.pearson08@gmail.com>
2023-09-10 12:18:24 -0600

    Added letscookbutton

--- main.js
+++ main.js
@@ -51,14 +51,17 @@ var dessert = [
 //DOM elements
 var letsCookButton = document.querySelector('.lets-cook');
 var randomSidesButton = document.querySelector('#side');
-var randomDessertButton = document.querySelector('#mainDish');
-var randomMainsButton = document.querySelector('#dessert');
+var randomDessertButton = document.querySelector('#dessert');
+var randomMainsButton = document.querySelector('#mainDish');
+var sideElement = document.querySelector('.dish-display');
+var cookPot = document.getElementById('#cookPotImage');
 
 //Event listeners
-letsCookButton.addEventListener("click",randomDish);
-randomSides.addEventListener("click",randomSideFun);
-randomDessert.addEventListener("click",randomDessertFun);
-randomMains.addEventListener("click",randomMainFun);
+//letsCookButton.addEventListener("click",randomDish);
+randomSidesButton.addEventListener("click",randomSideFun);
+randomDessertButton.addEventListener("click",randomDessertFun);
+randomMainsButton.addEventListener("click",randomMainFun);
+letsCookButton.addEventListener("click", hideCookPot);
 
 //Functions
 
@@ -76,5 +79,13 @@ function randomSideFun() {
     sideElement.innerHTML = randomMains;
 }
 
-  
+function hideCookPot () {    
+    if (sideElement.style.display === "none") {
+        sideElement.style.display = "block";
+        cookPot.style.display = "none";
+    } else {
+        sideElement.style.display = "none";
+        cookPot.style.display = "block";
+    }
+};
   
